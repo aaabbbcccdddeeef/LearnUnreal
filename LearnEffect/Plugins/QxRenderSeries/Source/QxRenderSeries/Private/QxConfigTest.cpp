8a4c283f9bd3d50338682b80c85e7409c396986d
monkey nobu <bbc724098@hotmail.com>
2022-06-28 21:09:05 +0800

    添加加载config的测试

---
+++ LearnEffect/Plugins/QxRenderSeries/Source/QxRenderSeries/Private/QxConfigTest.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QxConfigTest.h"
+
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AQxConfigTest::AQxConfigTest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AQxConfigTest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AQxConfigTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AQxConfigTest::TestConfigRead()
+{
+	if (!GConfig)
+	{
+		return;
+	}
+
+	FString ValueReceived;
+	GConfig->GetString(
+		TEXT("/Script/Engine.WorldInfo"), //Section
+		TEXT("GlobalDefaultGameType"), //Key
+		ValueReceived,
+		GGameIni //引擎初始化好的全局配置文件路径
+		);
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	check(PlayerController);
+	PlayerController->ClientMessage("GlobalDefaultGameType");
+	PlayerController->ClientMessage(ValueReceived);
+
+	int32 IntValueReceived;
+	GConfig->GetInt(
+		TEXT("Core.System"),
+		TEXT("MaxObjectsNotConsideredByGC"),
+		IntValueReceived,
+		GEngineIni
+		);
+
+	PlayerController->ClientMessage("MaxObjectsNotConsideredByGC");
+	PlayerController->ClientMessage(FString::FromInt(IntValueReceived));
+	
+}
+
+void AQxConfigTest::TestConfigWrite()
+{
+	if (!GConfig)
+	{
+		return;
+	}
+
+	// 添加新的section
+	FString QxTestSection = "QxConfig.Core";
+
+	GConfig->SetString(
+		*QxTestSection,
+		TEXT("RootDir"),
+		TEXT("E:/UE4/IsAwesome"),
+		GGameIni
+		);
+
+	GConfig->SetVector(
+		*QxTestSection,
+		TEXT("PlayerStartLocation"),
+		FVector(0, 0, 512),
+		GGameIni
+		);
+
+	GConfig->SetRotator(
+		*QxTestSection,
+		TEXT("SunRotation"),
+		FRotator(-90, 0, 0),
+		GGameIni
+		);
+
+	GConfig->Flush(false, GGameIni);
+}
+
+void AQxConfigTest::TestLoadConfig()
+{
+	LoadConfig();
+}
+
+void AQxConfigTest::TestSaveConfig()
+{
+	SaveConfig();
+}
+
---
+++ LearnEffect/Plugins/QxRenderSeries/Source/QxRenderSeries/Public/QxConfigTest.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "QxConfigTest.generated.h"
+
+UCLASS(Config=Game)
+class QXRENDERSERIES_API AQxConfigTest : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AQxConfigTest();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category="QxRenderSeires")
+	void TestConfigRead();
+
+	UFUNCTION(BlueprintCallable, Category="QxRenderSeires")
+	void TestConfigWrite();
+
+	UFUNCTION(BlueprintCallable, Category="QxRenderSeires")
+	void TestLoadConfig();
+
+	UFUNCTION(BlueprintCallable, Category="QxRenderSeires")
+	void TestSaveConfig();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(Config, BlueprintReadWrite, Category="QxRenderSeires")
+	FString TestStr;
+
+	UPROPERTY(Config, BlueprintReadWrite, Category="QxRenderSeires")
+	float TestFloat;
+};
