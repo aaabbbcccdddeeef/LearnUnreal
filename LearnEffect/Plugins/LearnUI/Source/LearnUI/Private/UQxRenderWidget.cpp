c14c45a02c36181ef1179ae4868450e4150e69d0
LUFFY <bbc724098@hotmail.com>
2022-08-19 01:29:08 +0800

    暂存添加ui渲染3d场景

---
+++ LearnEffect/Plugins/LearnUI/Source/LearnUI/Private/UQxRenderWidget.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UQxRenderWidget.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+#include "Widgets/Images/SImage.h"
+
+TSharedRef<SWidget> UUQxRenderWidget::RebuildWidget()
+{
+	RenderScene = MakeShareable(new FRenderWidgetPreviewScene());
+
+	USceneCaptureComponent2D* CaptureCom = NewObject<USceneCaptureComponent2D>();
+	const FTransform CaptureComponentTransform(
+		FRotator(0, 0, 0),
+		FVector(-100, 0, 50),
+		FVector::OneVector);
+
+	CaptureCom->TextureTarget = RenderTargetTexture;
+	RenderScene->AddComponent(CaptureCom, CaptureComponentTransform);
+
+	const FTransform MeshComponentTransform(FTransform::Identity);
+	UStaticMeshComponent* MeshComponent =
+		NewObject<UStaticMeshComponent>();
+
+	MeshComponent->SetStaticMesh(StaticMeshToRender);
+	RenderScene->AddComponent(MeshComponent, MeshComponentTransform);
+
+	CaptureCom->UpdateSceneCaptureContents(RenderScene->GetScene());
+
+	MyImage = SNew(SImage);
+	
+	return Super::RebuildWidget();
+}
+
+void UUQxRenderWidget::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+}
---
+++ LearnEffect/Plugins/LearnUI/Source/LearnUI/Public/UQxRenderWidget.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PreviewScene.h"
+#include "Components/Widget.h"
+#include "UObject/Object.h"
+#include "UQxRenderWidget.generated.h"
+
+
+class FRenderWidgetPreviewScene : public FPreviewScene
+{
+public:
+	
+};
+
+
+class SPriewScene : public  SImage
+{
+public:
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUI_API UUQxRenderWidget : public UWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="WorldAsset")
+	UTextureRenderTarget2D* RenderTargetTexture;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="WorldAsset")
+	UStaticMesh* StaticMeshToRender;
+
+protected:
+
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+	virtual void SynchronizeProperties() override;
+
+private:
+
+	TSharedPtr<FRenderWidgetPreviewScene> RenderScene;
+
+	TSharedRef<SImage> MyImage;
+};
+
+
