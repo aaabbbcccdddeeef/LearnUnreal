320cdfb8b9703157ff260c0bdeae62fb414e207a
LUFFY <bbc724098@hotmail.com>
2022-08-27 17:08:38 +0800

    新建一个学习ue5的工程

---
+++ LearnUE5/Source/LearnUE5/LearnUE5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearnUE5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUE5_API ALearnUE5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
